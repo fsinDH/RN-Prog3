21a84e8f1de55023825596321323a1fe9ddb6717
RoniGottfried <rgottfried@edu.ar>
2022-11-10 11:57:37 -0300

    posts

---
+++ src/screens/NewPost.js
@@ -0,0 +1,20 @@
+/* import React, { Component } from 'react';
+
+
+class NewPost extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            userActivo:{},
+            description:'',
+            showCamera:true,
+            loading:true,
+            uri:'',
+            likes:[],
+            comments:[]
+        }
+    }
+
+}
+
+export default NewPost; */
