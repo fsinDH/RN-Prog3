fd8cd477fb9b44ab4825cf6d16ef5f23eb9d4f12
Lpiwkoo11 <lpiwko@udesa.edu.ar>
2022-11-20 17:39:43 -0300

    busqueda, solo falta que encuentre usuarios

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,92 +1,90 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet, View, TouchableOpacity, TextInput, Text, FlatList} from 'react-native';
+import React, { Component } from 'react'
 import { db } from '../firebase/config';
-import SearchResults from './SearchResults'; 
+import { Text, TextInput, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
-export default class Search extends Component {
+export default class Busqueda extends Component {
+
     constructor(props){
         super(props)
-        this.state = {
-            busqueda: "",
-            resultado: [],
-            users: [],
-            error: "",
-            loading: true,
-            filterBy: '',
+        this.state={
+            busqueda:"",
+            cargando:true,
+            users:[],
+            resultados:[],
         }
-      
-     }
-componentDidMount(){
-    db.collection("users").onSnapshot(
-        users => {
-            let usersFromDb=[]
-            users.forEach((user) => {
-                usersFromDb.push({
-                    id: user.id,
-                    data: user.data()
-                })
-            }) 
-            this.setState({
-                users: usersFromDb
-            })
-            
-        })
     }
-
-   
-buscador(){
-    if(this.state.busqueda.length > 0){
-        
-        let nuevoArray = this.state.users.filter((user) => {
-          return  user.data.nombreUsuario.includes(this.state.busqueda)}
-        )
+    componentDidMount(){
+        db.collection("users").onSnapshot(docs=>{
+            let usersFromDB=[]
+            docs.forEach((doc)=>{
+               let users= doc.data();
+                usersFromDB.push({id:doc.id, data:users})
+            });
+            console.log(usersFromDB);
             this.setState({
-                resultado: nuevoArray,
-            
-            }, () => console.log(this.state.resultado))
-    
-            if(this.state.resultado.length== 0) {
-            this.setState({
-            error: "No existen coincidencias para este usuario"
+                users:usersFromDB,
+                cargando:false
             })
-            }   
-    } else if(this.state.busqueda.length == 0) {
-        this.setState({
-            error: "Este campo no puede estar vacío"
-        })
-    } 
-      
-}
 
-render() {
-    return (
-        <>
-        {this.state.resultado.length > 0 ?
-        <View>
-            <FlatList
-            data = {this.state.resultado}
-            keyExtractor= {item => item.id}
-            renderItem = { ({item}) => <SearchResults dataUser={item} 
-            {...this.props} />}>
-            </FlatList>
-        </View> : <Text>{this.state.error}</Text>
-        }
+            
+        })
+      
+
+    
+    }
+    buscador(){
+        if(this.state.busqueda.length > 0){
+            
+            let nuevoArray = this.state.users.filter((user) => {
+              return  user.data.nombreUsuario.includes(this.state.busqueda)}
+            )
+                this.setState({
+                    resultados: nuevoArray,
+                
+                }, () => console.log(this.state.resultados))
+        
+                if(this.state.resultados.length== 0) {
+                this.setState({
+                error: "No existe un usuario con ese nombre"
+                })
+                }   
+        } else if(this.state.busqueda.length == 0) {
+            this.setState({
+                error: "Este campo no puede estar vacío"
+            })
+        } 
+          
+    }
+    
+    render() {
+        return (
+            <>
+            {this.state.resultados.length > 0 ?
+            <View>
+                <FlatList
+                data = {this.state.resultados}
+                keyExtractor= {item => item.id}
+                renderItem = { ({item}) => <SearchResults dataUser={item} 
+                {...this.props} />}>
+                </FlatList>
+            </View> : <Text>{this.state.error}</Text>
+            }
+              <View>
+                
+                    <TextInput style={{}} placeholder="buscá a tus amigos"
+                    onChangeText={(text) => this.setState({busqueda: text})}>
+                    </TextInput>
+                    <TouchableOpacity onPress = {() => this.buscador()}> 
+                        <Text>buscar</Text>
+                    </TouchableOpacity>
+                    </View>
           <View>
             
-                <TextInput style={{}} placeholder="buscá a tus amigos"
-                onChangeText={(text) => this.setState({busqueda: text})}>
-                </TextInput>
-                <TouchableOpacity onPress = {() => this.buscador()}> 
-                    <Text>buscar</Text>
-                </TouchableOpacity>
-                </View>
-      <View>
-        
-        
-
-        
-        
-      </View></>
-    )
-  }
-}
+            
+    
+            
+            
+          </View></>
+        )
+      }
+    }
